bba0646f8d31fd9033629691a3ba160f35164dee
Jacob <jakesoto48@gmail.com>
2023-12-07 15:35:14 -0600

    added example function
    function shows an example of a for-loop, typecasting, and user input

--- hello_world.swift
+++ hello_world.swift
@@ -1,2 +1,22 @@
 
-print("Hello, World!")
+print("Hello, World!\n")
+
+print("Creating a function...")
+func addUserInput() -> Int{
+    var sum: Int = 0
+
+    for _ in 1...2 {
+        print("Enter an integer to add: ")
+
+        if let temp = Int(readLine()!) {
+            sum += temp
+        }
+        else {
+            print("The input is not a valid integer")
+        }
+    }
+    return sum
+}
+
+var mySum = addUserInput()
+print("Sum of user Input: \(mySum)")
