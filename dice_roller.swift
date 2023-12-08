91960cad5633762f444e9106452b079fb4e5ac2f
Jacob <jakesoto48@gmail.com>
2023-12-08 02:56:50 -0600

    Update dice_roller.swift
    working dice roller program

--- dice_roller.swift
+++ dice_roller.swift
@@ -1,12 +1,10 @@
 
+class Dice {
+    private var sides: [Int:String] = [:]
 
-struct Dice {
-    var sides: [Int] = []
-    var faces: [Int:String] = [:]
-
+    /* Constructor initializing a 6-sided die and its faces */
     init() {
-        self.sides = [1,2,3,4,5,6]
-        self.faces = [1:"-",
+        self.sides = [1:"⚀",
                     2:"⚁",
                     3:"⚂",
                     4:"⚃",
@@ -14,21 +12,40 @@ struct Dice {
                     6:"⚅"]
     }
 
+    /* If the index exists as a key in the dictionary, return the corresponding value,
+    Else return empty string */
     private func getFace(index: Int) -> String {
-        return self.faces[index]
+        let temp: String? = self.sides[index]
+        return temp ?? ""
     }
 
-    func printSides() {
-        for side in sides {
-            if let face = self.getFace(index: side){
-                print("\(face)")
-            }
-        }
-    }
 
+    /* Returns tuple containing the Integer value of the roll,
+    and the corresponding String representation of the dice face */
+    func rollDice() -> (Int, String) {
+        let num = Int.random(in: 1...sides.count)
+        return (num, self.getFace(index:num))
+    }
 
 }
 
 
-var dice = Dice()
-dice.printSides()
+
+/* Main Program */
+func main() {
+    var myDice: [Int:Dice] = [:]    // Dictionary matching Integer key to Dice object
+    var rollSum: Int = 0            // Integer to add the result of each dice roll
+
+    for i in 1...2 {
+        let currentDie = Dice()
+        myDice[i] = currentDie
+
+        let rollResult = currentDie.rollDice()
+        rollSum += rollResult.0
+        print("Roll \(i): \(rollResult.1)")
+    }
+
+    print("Sum of dice rolls: \(rollSum)")
+}
+
+main()
